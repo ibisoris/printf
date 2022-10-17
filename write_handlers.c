18d706d0772232233c0ca73222c2413917842ede
Kwabena Obeng <93714286+kobbyob123@users.noreply.github.com>
2022-10-17 21:14:15 +0000

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -5,7 +5,7 @@
  * handle_write_char - Prints a string
  * @c: char types.
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags.
+ * @flags:  Calculates the active flags.
  * @width: get width.
  * @precision: precision specifier
  * @size: Size specifier
